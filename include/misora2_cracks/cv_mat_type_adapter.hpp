1298d0f62de4f056ba74c7af773d552449123679
rui623 <ruiiwata837@gmail.com>
2025-09-15 10:52:18 +0900

    update model path and cmake

--- include/misora2_cracks/cv_mat_type_adapter.hpp
+++ include/misora2_cracks/cv_mat_type_adapter.hpp
@@ -5,6 +5,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include <sensor_msgs/msg/image.hpp>
 #include <cv_bridge/cv_bridge.hpp>
+// #include <cv_bridge/cv_bridge.h>
 #include <rclcpp/type_adapter.hpp>
 
 template<>
